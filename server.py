b08c782cc9a5d133abe77abb93e03fc0668340a2
MrMaxime1er <victormorabito@gmail.com>
2024-03-12 11:07:40 +0100

    feat: added proper exception handling

--- server.py
+++ server.py
@@ -42,73 +42,97 @@ class MyServer(BaseHTTPRequestHandler):
             self.do_post_collection()
     
     def do_DELETE(self):
-        if self.path.startswith('/collection/'):
+        if self.path.startswith('/collections/'):
             collection_id = self.path.split('/')[-1]
             self.do_delete_collection(collection_id)
 
 
+    def handle_error(self, code, message):
+        # Format error information into a JSON string
+        error_response = json.dumps({"code": str(code), "description": message})
+        
+        # Send the JSON response
+        self.send_response(code)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(bytes(error_response, "utf-8"))
 
     # Base route of the api
     def do_home(self):
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            self.wfile.write(bytes("<html><head></head>", "utf-8"))
-            self.wfile.write(bytes("<p>Request: This is the base route of the pyApi</p>", "utf-8"))
-            self.wfile.write(bytes("<body>", "utf-8"))
-            self.wfile.write(bytes("</body></html>", "utf-8"))
+            self.wfile.write(bytes("<html><head></head><p>Request: This is the base route of the pyApi</p>body></body></html>","utf-8"))
 
     # Get all collections
     def do_collections(self):
-        cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';")
-        fetched_collections = cursor.fetchall()
-        # Construct the JSON data
-        collections = [{'collection': row} for row in fetched_collections]
-        json_data = json.dumps(collections)
+        try:
+            cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';")
+            fetched_collections = cursor.fetchall()
+            # Construct the JSON data
+            collections = [{'collection': row} for row in fetched_collections]
+            json_data = json.dumps(collections)
 
-        # Send the HTTP response
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json_data, "utf-8"))
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(json_data, "utf-8"))
+        except Exception as e:
+            self.handle_error(500, 'Internal server error')
+
 
     
     def do_post_collection(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        print("POST request,\nPath: %s\nHeaders: %s\n\nBody: %s\n" % (self.path, self.headers, post_data.decode('utf-8')))
+        try:
+            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+            post_data = self.rfile.read(content_length) # <--- Gets the data itself
+            print("POST request,\nPath: %s\nHeaders: %s\n\nBody: %s\n" % (self.path, self.headers, post_data.decode('utf-8')))
 
-        data_dict = json.loads(post_data.decode('utf-8'))
-        title_lower = data_dict["title"].lower().replace(" ", "_")
-        
-        cursor.execute("DROP TABLE IF EXISTS public.%s" % title_lower)
-        cursor.execute("CREATE TABLE public.%s (id SERIAL PRIMARY KEY, title TEXT, updateFrequency integer, description TEXT, itemType TEXT)" % title_lower)
-        #cursor.execute("INSERT INTO public.moving_humans VALUES(DEFAULT, %s, %s, %s, %s)", (data_dict["title"], data_dict["updateFrequency"], data_dict["description"], data_dict["itemType"]))
-        connection.commit()
+            data_dict = json.loads(post_data.decode('utf-8'))
+            title_lower = data_dict["title"].lower().replace(" ", "_")
+            
+            cursor.execute("DROP TABLE IF EXISTS public.%s" % title_lower)
+            cursor.execute("CREATE TABLE public.%s (id SERIAL PRIMARY KEY, title TEXT, updateFrequency integer, description TEXT, itemType TEXT)" % title_lower)
+            #cursor.execute("INSERT INTO public.moving_humans VALUES(DEFAULT, %s, %s, %s, %s)", (data_dict["title"], data_dict["updateFrequency"], data_dict["description"], data_dict["itemType"]))
+            connection.commit()
 
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(post_data.decode('utf-8'), "utf-8"))
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(post_data.decode('utf-8'), "utf-8"))
+        except Exception as e:
+            self.handle_error(500, 'Internal server error')
     
     def do_collection_id(self, collectionId):
-        cursor.execute("SELECT * FROM public.%s;" % collectionId)
-        r = cursor.fetchall()
+        try:
+            cursor.execute("SELECT * FROM public.%s;" % collectionId)
+            r = cursor.fetchall()
 
-        res = json.dumps(r)
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(res.encode('utf-8'))
+            # Convert fetched data to JSON
+            res = json.dumps(r)
+
+            # Send response
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(res.encode('utf-8'))
+        except Exception as e:
+            # Handle any exceptions
+            print(e)
+
+
+            self.handle_error(404 if 'does not exist' in str(e) else 500, 'no collection was found' if 'does not exist' else 'Server internal error')
+
 
     def do_delete_collection(self, collectionId):
-        cursor.execute("DROP TABBLE IF EXISTS public.%s" %collectionId)
-        connection.commit()
-
-
-    
-
-
+        try:
+            cursor.execute("DROP TABLE IF EXISTS public.%s" % collectionId)
+            connection.commit()
+            self.send_response(204)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+        except Exception as e:
+            self.handle_error(500, str(e))
 
 
 
